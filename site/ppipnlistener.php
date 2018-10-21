a0d31fb338d3ed16fbe0f6d55a1ed978dd9524b2
Ted L <tedl57>
2018-10-21 04:28:42 +0100

    implemented and tested IPN listener communications with PayPal IPN Simulator todo: implement backend processing after payment verified

--- site/ppipnlistener.php
+++ site/ppipnlistener.php
@@ -15,8 +15,150 @@ jimport('joomla.application.component.controller');
 JLoader::registerPrefix('Ppipnlistener', JPATH_COMPONENT);
 JLoader::register('PpipnlistenerController', JPATH_COMPONENT . '/controller.php');
 
+// trlowe 10/18/18 convert this component into a server listener for PayPal IPN messages
+
+// PayPal POSTS IPN variables into listener
+
+$npostvars = 0;
+if ( isset( $_POST) )
+	$npostvars = count( $_POST );
+
+putLogEntry( "Session started; npostvars = $npostvars" );
+
+$logdetails = true;	// todo: make this a component option to turn on for debugging
+
+if ( $npostvars > 0 )
+{
+	if ( $logdetails )
+		putLogEntry( "_POST = " . var_export( $_POST, TRUE ) );
+}
+else
+	jexit();	// nothing to process - just exit
+
+// PayPal's IPN Simulator sets _POST["test_ipn"], whereas "live" transactions don't;  respond to the sandbox server or live server as appropriate
+
+$sandbox = "";
+if ( isset( $_POST["test_ipn"] ) )
+{
+	putLogEntry( "Testing in sandbox" );
+	$sandbox = ".sandbox";
+}
+
+// The reply back to PayPal includes an echo of all the POST'ed keys & values with a 'notify validate' command added
+
+$reply = 'cmd=_notify-validate';
+
+foreach( $_POST as $key => $value )
+{
+	$value = urlencode(stripslashes($value));
+	$reply .= "&$key=$value";
+}
+
+if ( $logdetails )
+	putLogEntry( "reply = $reply" );
+
+// prepare header for IPN POST back to PayPal server
+
+// ipn fix 6/30/2018, for details see: 
+// https://stackoverflow.com/questions/37589359/ipn-verification-postback-to-https
+
+$header = "POST /cgi-bin/webscr HTTP/1.1\r\n";	// ipn fix 6/30/2018 (was 1.0)
+$header .= "Content-Type: application/x-www-form-urlencoded\r\n";
+$header .= "Content-Length: " . strlen($reply) . "\r\n";
+// pre-ipn fix $header .= "Host: www$DEBUG.paypal.com:80\r\n";
+$header .= "Host: ipnpb$sandbox.paypal.com\r\n"; // ipn fix 6/30/2018
+$header .= "Connection: close\r\n\r\n"; // add for ipn fix 6/30/2018
+
+// open connection to PayPal server
+
+// pre-ipn fix was:
+// $fp = fsockopen ("www$DEBUG.paypal.com", 80, $errno, $errstr, 30);
+
+$fp = fsockopen( "tls://ipnpb$sandbox.paypal.com", 443, $errno, $errstr, 30); // ipn fix 6/30/2018
+
+// check connection status
+
+if ( !$fp )
+{
+	// HTTPS ERROR
+	// todo: handle this another way?
+
+	putLogEntry( "ERROR: Connection to PayPal server failed!" );
+
+	jexit();
+}
+
+// send IPN "post back" reply to PayPal server
+
+fputs( $fp, $header . $reply );	// todo: check return?
+
+// read response from PayPal server, looking for a VERIFIED response
+
+$verified = false;
+
+$response = "";
+
+while( !feof( $fp ) )
+{
+	$res = fgets( $fp, 1024 );
+
+	$response .= "$res|";
+
+	if (strcmp ($res, "VERIFIED"."\r\n" ) == 0) // todo: not sure why paypal is adding ^M (CR=\r) to all 
+	{
+		$verified = true;
+		putLogEntry( "IPN Verified!" );
+	}
+
+}
+
+if ( $logdetails )
+		putLogEntry( "IPN Response: $response" );
+
+// close connection with PayPal server
+
+fclose( $fp );
+
+// if the payment was not verified, log an error and quit
+if ( ! $verified )
+{
+		putLogEntry( "ERROR: IPN NOT Verified!" );
+
+		jexit();	// todo: what else to do?
+}
+
+// do backend processing of the verified payment
+// 1. check the transaction exists
+// 2. check the transaction is not already marked paid (or completed or cancelled?)
+// 3. check the transaction amount and the paid amount match
+// 4. check the custom or "items" match?
+// 5. if all the check pass, update the date_paid field in the DB (which will cause it to show in Payment Processor)
+
+// todo: implement this backend portion
+
+jexit();	// listener doesn't need to continue into joomla MVC pattern
 
 // Execute the task.
-$controller = JControllerLegacy::getInstance('Ppipnlistener');
-$controller->execute(JFactory::getApplication()->input->get('task'));
-$controller->redirect();
+//$controller = JControllerLegacy::getInstance('Ppipnlistener');
+//$controller->execute(JFactory::getApplication()->input->get('task'));
+//$controller->redirect();
+
+/////////////////////////////////////////////////////////////
+
+function putLogEntry( $txt )
+{
+	static $nlogentries = 1;
+
+	// object for adding log entries into the DB
+
+	$session = new stdClass();
+	$session->id = 0;	// set primary key for auto-increment on insert
+	$session->entry_time = date('Y-m-d H:i:s');
+
+	$session->entry_text = "(" . $nlogentries++ . ") $txt";
+
+	// insert the log entry object into the DB
+
+	$result = JFactory::getDbo()->insertObject('#__ppipnlistener_log',
+		$session, 'id');
+}
